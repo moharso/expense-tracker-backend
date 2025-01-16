9e8fcc798374e511bdacdaf932e39b53a7baca02
Karolina Masharo <karolinamasharo@gmail.com>
2025-01-16 20:26:51 +0200

    Testing husky and lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import routes from "./routes/ping";
-import config from "./config"
+import express from 'express';
+import routes from './routes/ping';
+import config from './config';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/ping", routes)
+app.use('/ping', routes);
 
 app.listen(config.port, () => {
-    console.log(`Server is running on port ${config.port}`)
-})
+  console.log(`Server is running on port ${config.port}`);
+});
 
 export default app;
