99830dadfdf242f426ca129d10e9759e5c659b64
Karolina Masharo <karolinamasharo@gmail.com>
2025-01-21 18:14:29 +0200

    Setting up SQLite Database (Raw Queries)

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import routes from './routes/ping';
 import config from './config';
+import expenseRoutes from './routes/expense';
+import { setupDatabase } from './db/db.service';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/ping', routes);
+
+setupDatabase();
+
+app.use('/expenses', expenseRoutes);
 
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
