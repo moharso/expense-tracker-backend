4fee0f4beeda667f3ebcdcc4af1af2038cad9da0
moharso <124620707+moharso@users.noreply.github.com>
2025-01-22 15:33:34 +0200

    Implementing Linting and Formatting (#3)
    * Setup environment: basic structure
    
    * Setted up basic REST API with one route
    
    * Testing husky and lint-staged
    
    * Testing husky and lint-staged
    
    * Implementing linting and formatting

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.json({ message: "pong"})
+  res.json({ message: 'pong' });
 });
 
 export default router;
