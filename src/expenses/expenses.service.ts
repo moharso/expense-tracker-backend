e7e5c0393262e5d1d00b6634ba676297785b8b33
Karolina Masharo <karolinamasharo@gmail.com>
2025-01-23 13:04:20 +0200

    Creating Endpoints for Getting Expenses

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,12 +1,25 @@
 import expensesRepository from './expenses.repository';
+import { Expense } from './entity/expense.entity';
 
-const createExpense = async (data: any) => {
+const createExpense = async (data: Expense) => {
   return await expensesRepository.create(data);
 };
 
-const getExpenses = async () => {
-  return await expensesRepository.findAll();
+const getExpenses = async (query: {
+  limit?: number;
+  offset?: number;
+  fromDate?: Date;
+  toDate?: Date;
+}) => {
+  return await expensesRepository.findAll(query);
 };
 
+const getExpenseById = async (id: number) => {
+  const expense = await expensesRepository.findById(id);
+  if (!expense) {
+    throw new Error(`Expense with ID ${id} not found`);
+  }
+  return expense;
+};
 
-export default { createExpense, getExpenses };
+export default { createExpense, getExpenses, getExpenseById };
--- src/helpers/middlewares/validator.ts
+++ src/helpers/middlewares/validator.ts
@@ -1,11 +1,44 @@
-import { validationResult } from 'express-validator';
+import { validationResult, query, param } from 'express-validator';
 import { Request, Response, NextFunction } from 'express';
-import  Exception from "../Exception"
+import Exception from '../Exception';
 
-export const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+export const validateRequest = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new Exception(errors.array().map(err => err.msg).join(', '), 400);
+    throw new Exception(
+      errors
+        .array()
+        .map((err) => err.msg)
+        .join(', '),
+      400,
+    );
   }
   next();
 };
+
+export const getExpensesValidation = [
+  query('limit')
+    .optional()
+    .isInt({ gt: 0 })
+    .withMessage('Limit must be a positive integer'),
+  query('offset')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Offset must be a positive integer'),
+  query('fromDate')
+    .optional()
+    .isISO8601()
+    .withMessage('fromDate must be a valid ISO date'),
+  query('toDate')
+    .optional()
+    .isISO8601()
+    .withMessage('toDate must be a valid ISO date'),
+];
+
+export const getExpenseByIdValidation = [
+  param('id').isInt().withMessage('ID must be a valid integer'),
+];
