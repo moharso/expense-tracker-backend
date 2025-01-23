fddf63f73f94b0cb2417d42576e8a517a41a62e0
Karolina Masharo <karolinamasharo@gmail.com>
2025-01-23 12:22:38 +0200

    Fixing typo

--- src/expenses/entity/expense.entity.ts
+++ src/expenses/entity/expense.entity.ts
@@ -1,17 +1,8 @@
-export class Expense {
-  id: number;
+export interface Expense {
+  id?: number;
   name: string;
   amount: number;
   currency: string;
   category: string;
   date: Date;
-
-  constructor(id: number, name: string, amount: number, currency: string, category: string, date: Date) {
-    this.id = id;
-    this.name = name;
-    this.amount = amount;
-    this.currency = currency;
-    this.category = category;
-    this.date = date;
-  }
 }
